11a1788238a32292af00b19549e024f3cf253690
aarb1 <abahendwar@gmail.com>
2018-10-14 15:47:57 -0400

    Setup basic character with movement

---
+++ CS4482-APP2-AB/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    private Animator characterAnimator;
+    private CharacterController characterController;
+
+    [SerializeField] //Can be edited in the inspector
+    private float moveSpeed = 350f;
+    private float rotationSpeed = 5f;
+
+    private void Awake() {
+        characterController = GetComponent<CharacterController>();
+        characterAnimator = GetComponentInChildren<Animator>();
+    }
+
+    void Update () {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+        var movement = new Vector3(horizontal, 0, vertical);
+
+        characterController.SimpleMove(movement * Time.deltaTime * moveSpeed);
+        characterAnimator.SetFloat("Speed", movement.magnitude);
+
+        if (movement.magnitude > 0) {
+            Quaternion facingDirection = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Slerp(transform.rotation, facingDirection, Time.deltaTime * rotationSpeed);
+        }
+    }
+}
